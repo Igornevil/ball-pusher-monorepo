5081148dc540761ee3e4662cac2ea26aa266f4b6
Igor Kiselov <igornevill@gmail.com>
2025-09-14 19:03:58 +0300

    fi build

--- packages/frontend/src/services/socket.ts
+++ packages/frontend/src/services/socket.ts
@@ -1,21 +1,42 @@
 import { io } from 'socket.io-client';
 
-const PRODUCTION_URL = import.meta.env.VITE_BACKEND_URL;
+const getServerUrl = () => {
+  if (typeof window !== 'undefined') {
+    const protocol = window.location.protocol;
+    const hostname = window.location.hostname;
+    if (hostname === 'localhost' || hostname === '127.0.0.1') {
+      return 'http://localhost:3000';
+    }
 
-const DEVELOPMENT_URL = 'http://localhost:3000';
-const SERVER_URL = PRODUCTION_URL || DEVELOPMENT_URL;
+    return `${protocol}//${hostname}:3000`;
+  }
 
-console.log(`[Socket] Спроба підключення до сервера за адресою: ${SERVER_URL}`);
+  return 'http://localhost:3000';
+};
 
-export const socket = io(SERVER_URL, {
-  transports: ['websocket'],
+export const socket = io(getServerUrl(), {
+  transports: ['websocket', 'polling'],
+  withCredentials: true,
 });
 
-socket.on('connect', () => {
-  console.log(`[Socket] ✅ Успішно підключено!
- ID: ${socket.id}`);
-});
+// import { io } from 'socket.io-client';
 
-socket.on('connect_error', (err) => {
-  console.error('[Socket] ❌ Помилка підключення', err.message, err.cause);
-});
+// const PRODUCTION_URL = import.meta.env.VITE_BACKEND_URL;
+
+// const DEVELOPMENT_URL = 'http://localhost:3000';
+// const SERVER_URL = PRODUCTION_URL || DEVELOPMENT_URL;
+
+// console.log(`[Socket] Спроба підключення до сервера за адресою: ${SERVER_URL}`);
+
+// export const socket = io(SERVER_URL, {
+//   transports: ['websocket'],
+// });
+
+// socket.on('connect', () => {
+//   console.log(`[Socket] ✅ Успішно підключено!
+//  ID: ${socket.id}`);
+// });
+
+// socket.on('connect_error', (err) => {
+//   console.error('[Socket] ❌ Помилка підключення', err.message, err.cause);
+// });
